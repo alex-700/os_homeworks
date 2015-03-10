1c2f13e993bd1a6bdb56b72dcdf0cec3dc278610
alex-700 <alex_700_95@mail.ru>
2015-03-10 23:45:24 +0300

    all done

--- cat/cat.c
+++ cat/cat.c
@@ -1,18 +1,25 @@
-#include"../lib/helpers.h"
+#include<helpers.h>
 #include<stdio.h>
+
 const size_t CHUNK_SIZE = 2048;
 
 int main()
 {
     char buf[CHUNK_SIZE];
-    ssize_t c;
+    ssize_t read_bytes, wrote_bytes;
+    
     do {
-        c = read_(STDIN_FILENO, buf, CHUNK_SIZE);
-        if (c == -1) {
-            printf("Fail");
-            break;
+        read_bytes = read_(STDIN_FILENO, buf, CHUNK_SIZE);
+        if (read_bytes == -1) {
+            perror("input fail");
+            return -1; 
         }
-        write_(STDOUT_FILENO, buf, c);
-    } while (c != 0);
+        wrote_bytes = write_(STDOUT_FILENO, buf, read_bytes);
+        if (wrote_bytes == -1) {
+            perror("output fail");
+            return -1;
+        }
+    } while (read_bytes != 0);
+    
     return 0;
 }

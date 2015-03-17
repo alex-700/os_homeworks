2884c6ddb382f6d0dc024554bacaca97adfa0b27
alex-700 <alex_700_95@mail.ru>
2015-03-18 02:10:46 +0300

    all done

--- lib/helpers.c
+++ lib/helpers.c
@@ -30,16 +30,21 @@ ssize_t write_(int fd, const void *buf, size_t count)
 }
 ssize_t read_until(int fd, void * buf, size_t count, char delimiter)
 {
-    
-    ssize_t c; 
+    ssize_t c;
     ssize_t ans = 0;
+    ssize_t last_ans = 0;
     do {
-        c = read(fd, buf + ans, 1);
+        c = read(fd, buf + ans, count);
         if (c == -1) {
             return -1;
         }
-        count-=c;
-        ans+=c;
-    } while (count != 0 && c != 0 && (*((char*) (buf + ans - 1)) != delimiter));
+        count -= c;
+        ans += c;
+        for (;last_ans < ans; last_ans++) {
+            if (*((char *)(buf + last_ans)) == delimiter) {
+                break;
+            }
+        }        
+    } while (c != 0 && count != 0 && last_ans == ans);
     return ans;
 }
--- revwords/revwords.c
+++ revwords/revwords.c
@@ -2,27 +2,51 @@
 #include<stdio.h>
 
 
-const size_t MAX_WORD_SIZE = 4096;
+const size_t MAX_WORD_SIZE = 4097;
 
 int main() 
 {
     char buf[MAX_WORD_SIZE];
-    ssize_t read_bytes, wrote_bytes;
+    ssize_t read_bytes, wrote_bytes, last_k, start = 0;
     do {
-        read_bytes = read_until(STDIN_FILENO, buf, MAX_WORD_SIZE, ' ');
+        read_bytes = read_until(STDIN_FILENO, buf + start, MAX_WORD_SIZE, ' ');
         if (read_bytes == -1) {
             perror("input fail");
             return -1;
         }
-        for (ssize_t i = 0, j =  read_bytes - (buf[read_bytes - 1] == ' ' ? 2 : 1); i < j; i++, j--) {
+        last_k = 0;
+        for (ssize_t k = start; k < start + read_bytes; k++) {
+            if (buf[k] == ' ') {
+                for (ssize_t i = last_k, j = k - 1; i < j; i++, j--) {
+                    char temp = buf[i];
+                    buf[i] = buf[j];
+                    buf[j] = temp;            
+                }
+                wrote_bytes = write_(STDOUT_FILENO, buf + last_k, k - last_k + 1);
+                if (wrote_bytes == -1) {
+                    perror("output fail");
+                    return -1;
+                }
+                last_k = k + 1;
+            }
+        }
+        if (last_k != 0) {
+            for (ssize_t i = last_k; i < start + read_bytes; i++) {
+                buf[i - last_k] = buf[i];
+            }
+        }
+        start += read_bytes - last_k;
+    } while (read_bytes != 0);
+    if (start != 0) {
+        for (ssize_t i = 0, j = start - 1; i < j; i++, j--) {
             char temp = buf[i];
             buf[i] = buf[j];
             buf[j] = temp;            
         }
-        wrote_bytes = write_(STDOUT_FILENO, buf, read_bytes);
+        wrote_bytes = write_(STDOUT_FILENO, buf, start);
         if (wrote_bytes == -1) {
             perror("output fail");
             return -1;
         }
-    } while (read_bytes != 0);
+    }
 }

b8adf523ed39ac69d3e7f4395bd8bb4fb0b57a8b
alex-700 <alex_700_95@mail.ru>
2015-03-24 23:19:18 +0300

    add ifndef to lib header, remove unnecessary files

--- lib/helpers.h
+++ lib/helpers.h
@@ -1,3 +1,6 @@
+#ifndef HELPERS_H
+#define HELPERS_H
+
 #include <unistd.h>
 #include <sys/wait.h>
 #include <sys/types.h>
@@ -6,3 +9,5 @@ ssize_t read_(int fd, void *buf, size_t count);
 ssize_t write_(int fd, const void *buf, size_t count);
 ssize_t read_until(int fd, void * buf, size_t count, char delimiter);
 int spawn(const char* file, char* const argv[]);
+
+#endif // HELPERS_H
